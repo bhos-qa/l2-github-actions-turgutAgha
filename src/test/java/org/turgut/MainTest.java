7a80aa15f6b2973157000bf3d62ceba072cc3151
turgutAgha <turgut.aghashov.std@bhos.edu.az>
2022-09-24 13:25:25 +0400

    some changes

--- src/test/java/org/turgut/MainTest.java
+++ src/test/java/org/turgut/MainTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 
-    Main main = new Main();
-
     @Test
     void testStringLength(){
         assertEquals("Length of the word 'TEsting1241' is 11", Main.stringLength("TEsting1241"));
